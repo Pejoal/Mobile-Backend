b7f310bfb8fbe40970f186feff8a423720573446
pejoal <pejoal.official@gmail.com>
2023-05-14 22:09:38 +0200

    Register / Login Functionality

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -3,26 +3,31 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 
-class RegisterController extends Controller
-{
-    public function register(Request $request) {
-      
-      $validated = $request->validate([
-        'name' => ['required'],
-        'email' => ['required'],
-        'password' => ['required', 'confirmed'],
-      ]);
+class RegisterController extends Controller {
+  public function register(Request $request) {
 
-      return User::create([
-        ...$validated,
-        'password' => bcrypt($request->password),
-      ]);
-    }
+    $validated = $request->validate([
+      'name' => ['required'],
+      'email' => ['required'],
+      'password' => ['required', 'confirmed'],
+    ]);
 
-    public function login() {
+    return User::create([
+      ...$validated,
+      'password' => bcrypt($request->password),
+    ]);
+  }
 
-    }
+  public function login(LoginRequest $request) {
+
+    $request->authenticate();
+
+    return response(['token' => auth()->user()->createToken($request->device_name)->plainTextToken]);
+
+  }
+
 }
